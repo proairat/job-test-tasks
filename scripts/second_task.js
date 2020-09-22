b639a7ae4daaebc5098bfe3c9198abfb9c9c1bc0
Shayakhmetov Airat <proairat@gmail.com>
2020-09-22 16:12:38 +0500

    Экспериментировал с second_task.js. В итоге оставил так как было при предыдущем коммите

--- scripts/second_task.js
+++ scripts/second_task.js
@@ -3,7 +3,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const text_in_paragraph = document.body.querySelector('p').textContent;     // считываем текстовое содержимое тега <p>
-    const red_word = text_in_paragraph.replace(/(?=r)(\b[A-Za-z]+\b)/gi, `<span class="red_color">$1</span>`);  // оборачиваем вхождения, содержащие первую регистронезависимую букву "r" | "R" 
+    const red_word = text_in_paragraph.replace(/(?=r)(\b[A-Za-z]+\b)/gi, '<span class="red_color">$1</span>');  // оборачиваем вхождения, содержащие первую регистронезависимую букву "r" | "R" 
                                                                                                                 // тегом <span> с классом, изменяющим цвет текста
 
     document.body.insertAdjacentHTML('beforeend', `<p>${red_word}</p>`);    // вставляем в body полученное преобразование исходного текста
