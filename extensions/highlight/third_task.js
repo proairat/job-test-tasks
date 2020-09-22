deeae85c1ea202c23561dae690e5695d6fa43dcf
Shayakhmetov Airat <proairat@gmail.com>
2020-09-22 23:24:06 +0500

    Обнаружил ошибку, связанную с вложенными тегами: заменялось значение атрибутов href и class. Исправил

--- extensions/highlight/third_task.js
+++ extensions/highlight/third_task.js
@@ -2,47 +2,22 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 	
-	/*const get_children = (element) => {
-
-		const children = [...element.children];
-
-		if (children.length > 0) {
-
-			children.map(elem => {
-
-				console.log(elem);
-
-				if (elem.children) {
-
-					get_children(elem);
-				}
-			})
-		}
-	}*/
-
 	document.body.addEventListener('click', () => {
 
 		chrome.tabs.executeScript(null, {
 
 			code: `!(() => {
 			
-				Array.from(document.body.children).forEach((item) => {
+						Array.from(document.body.children).forEach((item) => {
 
-					const colored_words = item.textContent.replace(/(\\b[A-Za-z]+\\b)(?<=ing)/gi, '<span style="color:#ff0000; font-weight:600;">$1</span>');  // оборачиваем вхождения, содержащие слова, заканчивающиеся на "ing" тегом <span> с классом, изменяющим цвет текста
-
-					item.innerHTML = colored_words;
-
-				});
+							item.innerHTML = item.innerHTML.replace(/(?<!((href|class).+))(\\b[A-Za-z]+\\b)(?<=ing)/gi, '<span style="color:#ff0000; font-weight:600;">$3</span>');  // оборачиваем вхождения, содержащие слова, заканчивающиеся на "ing" тегом <span> с классом, изменяющим цвет текста
+						});
 				
-			})();`
+					})();`
 
 		});
-
-		window.close();
-
 	});
 
 	document.body.dispatchEvent(new Event("click"));
 
 });
-
